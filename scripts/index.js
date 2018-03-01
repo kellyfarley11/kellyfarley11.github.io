f294dfc55a2cd1792ef475dbd95f9f9ce71072e8
Kelly Farley <farleykc@gmail.com>
2018-02-28 21:14:34 -0600

    added final text to questions and answers in html

--- scripts/index.js
+++ scripts/index.js
@@ -4,38 +4,34 @@ var time = 0;
 
 setInterval(function(){
 	time = time + 1;
-	if (time>3){
-		console.log("yes");
-	}
+	$("#timer").html(time);
 }, 1000);
 
-$("p").on("click",function(){
+/* $("p").on("click",function(){
   $("body").css("color", "#990000");
 });
+ */
 
-var answerTotal = 0;
 
-/* $("#HTMLanswer").on("click", function(){
-	answerTotal = answerTotal + 1;
+$(".HTMLanswer").on("click", function(){
+  var newCurrentTotal = parseInt($("#total").html()) + 1;
+  $("#total").html(newCurrentTotal);
 });
 
+
+/* 
 $("#CSSanswer").on("click", function(){
 	answerTotal = answerTotal + 2;
 });
 
 $("#JSanswer").on("click", function(){
 	answerTotal = answerTotal + 3;
-});
+}); */
 
-if (answertotal=9){
-	$("body").css("background-color", "#990000");
-} */
-
+/*
 function addToAnswerTotal(x){
 	answerTotal = answerTotal + x;
 	if answerTotal > 9 {
 		$("body").css("background-color", "#990000");
 	}
-}
-
-addToAnswerTotal(10);
+} */
